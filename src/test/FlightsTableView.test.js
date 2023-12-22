c6e8f55716f7946a5a8ab4b7554131247db0c448
kimz code <81085401+KimzCodes@users.noreply.github.com>
2023-12-22 09:38:56 +0200

    done

--- src/test/FlightsTableView.test.js
+++ src/test/FlightsTableView.test.js
@@ -2,11 +2,11 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import configureStore from "redux-mock-store";
-import FlightsTableView from "../FlightsTableView/FlightsTableView";
+import FlightsTableView from "../components/flights/FlightsTableView/FlightsTableView";
 
 const mockStore = configureStore([]);
 
-it("should render table rows with Code, Capacity, Departure Date, and Image", () => {
+it("should render table rows with Code, Capacity, Departure Date", () => {
   const records = [
     {
       id: 1,
